6c0931ff259944fbbb45cfc184b0188b7693aadd
Alex Gor <ifresh.wp@gmail.com>
2014-07-27 01:39:30 +0400

    server: now server can listen.

--- server.cpp
+++ server.cpp
@@ -3,7 +3,6 @@
 #include "log.h"
 
 #include <string>
-#include <WinSock2.h>
 
 namespace server
 {
@@ -13,7 +12,10 @@ int Server::servers = 0;
 
 // ---------------------------------------------
 
-Server::Server(void)
+Server::Server( u_short port )
+	:listenSock(0)
+	,isListen(false)
+	,listenThread(0)
 {
 	if ( servers < 0 )
 		throw Exception( std::string( "Negative number of working servers." ), 
@@ -22,9 +24,9 @@ Server::Server(void)
 
 	if ( servers < MAX_SERVERS )
 	{
-		const WORD requestedVersion = MAKEWORD( 2, 2 );
+		const WORD requestedVersion = MAKEWORD( LO_VER, HI_VER );
         WSADATA wsaData;
-		const int errorCode = WSAStartup( requestedVersion, &wsaData );
+		int errorCode = WSAStartup( requestedVersion, &wsaData );
 
 		if ( errorCode )
 		{
@@ -34,8 +36,31 @@ Server::Server(void)
 		}
 		else
 		{
+			if ( LOBYTE( wsaData.wVersion ) != LO_VER || HIBYTE( wsaData.wVersion ) != HI_VER )
+				throw Exception( std::string( "WinSoc DLL is not support requested version." ), 
+								 std::string( "server.cpp : Server::Server(void)" ), 
+								 Exception::ExcData( &requestedVersion, sizeof( requestedVersion ) ) );
+
 			++servers;
-			Log::getLog() << std::string( "started new server #" ).append( std::to_string( servers ) );
+			Log::getLog() << std::string( "winsock was initialized" );
+
+			if ( ( listenSock = socket( AF_INET, SOCK_STREAM, 0) ) < 0 )
+				throw Exception( std::string( "Error while creating socket." ), 
+								 std::string( "server.cpp : Server::Server(void)" ), 
+								 Exception::ExcData( &listenSock, sizeof( listenSock ) ) );
+			Log::getLog() << std::string( "listen socket was initialized" );
+
+			sockaddr_in local_addr;
+            local_addr.sin_family = AF_INET;
+            local_addr.sin_port = htons( port );
+            local_addr.sin_addr.s_addr = 0;
+
+			if ( ( errorCode = bind( listenSock, ( sockaddr* )&local_addr, sizeof( local_addr ) ) ) )
+				throw Exception( std::string( "Error while binding socket." ), 
+								 std::string( "server.cpp : Server::Server(void)" ), 
+								 Exception::ExcData( &errorCode, sizeof( errorCode ) ) );
+			Log::getLog() << std::string( "listen socket was bound" );
+			Log::getLog() << std::string( "server was initialized" );
 		}
 	}
 	else
@@ -46,7 +71,67 @@ Server::Server(void)
 
 Server::~Server(void)
 {
+	if ( listenSock )
+		 closesocket( listenSock );
 
+	--servers;
+	Log::getLog() << std::string( "server shutdown. Still working: " ).append( std::to_string( servers ) );
+
+	if ( servers == 0 )
+		WSACleanup();
+}
+
+// ---------------------------------------------
+
+std::thread* Server::listenGo( int queue_size )
+{
+	if ( isListen )
+		return listenThread;
+
+	MUTEXED_ASSIGN( isListenMutex, isListen, true );
+	listenThread = new std::thread( &Server::listenState, this );
+
+	return listenThread;
+}
+
+void Server::listenStop( void )
+{
+	if ( !isListen )
+		return;
+
+    MUTEXED_ASSIGN( isListenMutex, isListen, false );
+	listenThread = 0;
+}
+
+void Server::listenState( void )
+{
+	int errorCode;
+
+	while ( isListen )
+	{
+		if ( errorCode = listen( listenSock, QUEUE_SIZE ) )
+		{
+			MUTEXED_ASSIGN( isListenMutex, isListen, false );
+			throw Exception( std::string( "Error while trying listen socket." ), 
+				             std::string( "server.cpp : void Server::listenGo( void )" ), 
+							 Exception::ExcData( &errorCode, sizeof( errorCode ) ) );
+		}
+
+		SOCKET client_socket;   
+        sockaddr_in client_addr;    
+        int client_addr_size = sizeof( client_addr );
+
+		while( isListen && ( client_socket = accept(listenSock, ( sockaddr* )&client_addr, &client_addr_size ) ) )
+        {
+			Log::getLog() << "looks like somebody connected";
+
+            HOSTENT* host;
+            host = gethostbyaddr( ( char* )&client_addr.sin_addr.s_addr, 4, AF_INET );
+			Log::getLog() << std::string( "connected: " ).append( host->h_name ) \
+				                                         .append( " " )          \
+														 .append( inet_ntoa( client_addr.sin_addr ) );
+		}
+	}
 }
 
 }
--- server.h
+++ server.h
@@ -1,17 +1,38 @@
 #pragma once
 
+#include <WinSock2.h>
+#include <mutex>
+#include <thread>
+
+#define MUTEXED_ASSIGN(mutex, a, b) { mutex.lock(); a = b; mutex.unlock(); }
+
 namespace server
 {
 
 static const int MAX_SERVERS = 32;
+static const int HI_VER = 2;
+static const int LO_VER = 2;
+static const int QUEUE_SIZE = 100;
 
 class Server
 {
 public:
-	Server(void);
+	Server( u_short port );
 	~Server(void);
+
+	std::thread* listenGo( int queue_size = QUEUE_SIZE );
+	void listenStop( void );
+private:
+	void listenState( void );
 private:
 	static int servers;
+
+	SOCKET listenSock;
+
+	bool isListen;
+	std::mutex isListenMutex;
+
+	std::thread* listenThread;
 };
 
 }
